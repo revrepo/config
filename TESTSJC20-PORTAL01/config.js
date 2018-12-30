df6b7c7f5dd7d0f0d0d094b959d3e7f9adc63c8e
Victor Gartvich <victor@TESTSJC20-PORTAL01.REVSW.NET>
2018-12-30 19:37:54 +0000

    TESTSJC20-PORTAL01 config

---
+++ TESTSJC20-PORTAL01/config.js
@@ -0,0 +1,12 @@
+
+window.API_URL = 'https://testsjc20-api01.revsw.net/v1';
+// window.API_URL = 'https://api.revsw.net/v1';
+
+window.gaAccount = 'UA-68856309-2';
+
+// Configuration parameter to set timeout value for POST and PUT requests to the API
+window.resourcePostPutTimeout = 180 * 1000;
+
+// Intro configuration flag (true=on, false=off)
+window.intro = true;
+
